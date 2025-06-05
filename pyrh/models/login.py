961534a34886b71f17033152a10cc5e03006a8a0
Vignesh Rao <svignesh1793@gmail.com>
2025-06-04 19:15:35 -0500

    Pickle the token while storing in local cache

--- pyrh/models/login.py
+++ pyrh/models/login.py
@@ -1,7 +1,7 @@
 """This module contains the caching logic for Robinhood login data."""
 
-import json
 import pathlib
+import pickle
 import time
 from datetime import datetime
 from typing import Union
@@ -17,8 +17,8 @@ Creates the directory on import.
 """
 LOGIN_ROOT.mkdir(parents=True, exist_ok=True)
 
-LOGIN_FILE: pathlib.Path = LOGIN_ROOT.joinpath("login.json")
-"""Path to login.json config file.
+LOGIN_FILE: pathlib.Path = LOGIN_ROOT.joinpath("login.pickle")
+"""Path to login.pickle file.
 
 Creates the file on import.
 """
@@ -28,8 +28,8 @@ LOGIN_FILE.touch(exist_ok=True)
 def load_existing_oauth() -> Union[OAuth, None]:
     """Get the path to the login file."""
     try:
-        with open(LOGIN_FILE) as file:
-            json_data = json.load(file)
+        with LOGIN_FILE.open("rb") as file:
+            json_data = pickle.load(file)
         assert isinstance(json_data, dict), "Cached data must be a dictionary."
         assert json_data.get("login"), "Cached data must contain 'login' key."
         assert json_data.get("expiry"), "Cached data must contain 'expiry' key."
@@ -41,7 +41,7 @@ def load_existing_oauth() -> Union[OAuth, None]:
     except AssertionError as error:
         print(f"Invalid cached data: {error}")
         raise InvalidCacheFile("Cached login file is invalid or corrupted.")
-    except (FileNotFoundError, json.JSONDecodeError):
+    except (FileNotFoundError, EOFError):
         return None
     else:
         return OAuthSchema().load(json_data["login"])
@@ -56,6 +56,6 @@ def save_existing_oauth(data: OAuth) -> None:
         "expiry": expiry,
         "expiration_dt": expiration_dt,
     }
-    with open(LOGIN_FILE, "w") as file:
-        json.dump(login_data, file, indent=4)
+    with LOGIN_FILE.open("wb") as file:
+        pickle.dump(login_data, file)
     print(f"Login data saved to {LOGIN_FILE} valid until {expiration_dt}.")
